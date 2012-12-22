57cea4ef0f62026ef0e913e7d024ccc15db8c975
Subhash Chandra <TMaYaD@LoonyB.in>
2012-12-22 11:05:39 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DomainHunt::Application.config.secret_token = '4a19b5eb3053ca09d6583f0e2f45d7f1c91b809fdbc12553d32a9086fe08f427d439f34b5547ad2a4770bf078988b60ef747b28e8803040169481035514bd761'
