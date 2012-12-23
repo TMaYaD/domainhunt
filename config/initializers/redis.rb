8037c82a460cb302ccd18a83e8852d048f99ef00
Subhash Chandra <TMaYaD@LoonyB.in>
2012-12-23 16:24:22 +0530

    support multiple redis databases

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,9 @@
 require 'redis' unless defined? Redis
+
+redis_config = YAML.load_file(Rails.root + 'config/redis.yml')[Rails.env] || {}
+
 begin
-  REDIS ||= Redis.new
+  REDIS ||= Redis.new redis_config
 rescue => e
   LOGGER.error "#{e.class.name}: #{e.message}"
   LOGGER.warn "couldn't load redis, this could be common in precompilation stage"
